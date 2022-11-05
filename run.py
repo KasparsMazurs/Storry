7ca8acc8f1c256bc90364bc6bd2844736e1809d0
KasparsMazurs <kaspars.mazurs@gmail.com>
2022-11-05 15:45:23 +0000

    Write an additional line in addition and subtraction function to check if the answer is valid

--- run.py
+++ run.py
@@ -13,12 +13,20 @@ def addition():
     c = a + b
     while True:
         answer = input(f'Can you solve this? \n {a} + {b} = ?\n')
-        if c == int(answer):
+        # Check if answer is valid
+        try:
+            answer = int(answer)
+        except:
+            print(f"Can you please count to me till {answer}\n NO! \n Then, please provide me with number!")
+            continue
+        # Check if answer is correct
+        if c == answer:
              print(f'Your right, answer was {answer}!\n')
              break
         else:
             print(f"That's wrong!\n")
             print(f"Think carefully!\n")
+
 
 def subtraction():
     """
@@ -26,8 +34,7 @@ def subtraction():
     """
     a = random.randint(1, 10)
     b = random.randint(1, 10)
-    print(a)
-    print(b)
+    # Make sure that answer is a positive number
     if a < b:
         c = b - a
     else:
@@ -36,8 +43,21 @@ def subtraction():
     while True:
         if a < b:
             answer = input(f'Can you solve this? \n {b} - {a} = ?\n')
+            # Check if answer is valid
+            try:
+                answer = int(answer)
+            except:
+                print(f"Can you please count to me till {answer}\n NO! \n Then, please provide me with number!")
+                continue
         else:
             answer = input(f'Can you solve this? \n {a} - {b} = ?\n')
+            # Check if answer is valid
+            try:
+                answer = int(answer)
+            except:
+                print(f"Can you please count to me till {answer}\n NO! \n Then, please provide me with number!")
+                continue
+        # Check if answer is correct
         if c == int(answer):
              print(f'Your right, answer was {answer}!\n')
              break
@@ -45,4 +65,5 @@ def subtraction():
             print(f"That's wrong!\n")
             print(f"Think carefully!\n")
 
+addition()
 subtraction()
