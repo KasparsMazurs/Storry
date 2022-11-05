d56b19b4a88dae6873850ed7fb686ef753b1911b
KasparsMazurs <kaspars.mazurs@gmail.com>
2022-11-05 13:07:32 +0000

    Initial commit

--- run.py
+++ run.py
@@ -1,3 +1,11 @@
 # Your code goes here.
 # You can delete these comments, but do not change the name of this file
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
+
+import random
+
+number_list = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+a = random.choice(number_list)
+b = random.choice(number_list)
+print(a)
+print(b)
